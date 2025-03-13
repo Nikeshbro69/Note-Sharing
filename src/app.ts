b84148f5a4a1fdb031a771504ec8f0af12302c68
Nikeshbro69 <kingnikesh038@gmail.com>
2025-03-14 00:42:31 +0545

    cors + backend completed

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,23 @@ import express from 'express'
 import globalErrorHandler from './middlewares/globalErrorHandler'
 // import { createNote } from './note/noteController'
 import noteRoute from './note/noteRoute'
+import envConfig from './config/config'
+import cors from 'cors'
 
 
 const app = express()
+// parse incoming JSON to handle Undefined error 
 app.use(express.json())
 
+//Cors configuration:
+app.use(cors({
+    origin : envConfig.frontendurl
+}))
+
 app.use("/api/notes",noteRoute)
+
+//Image public
 app.use(express.static("./src/uploads"))
+//Global error handler
 app.use(globalErrorHandler) //Every time call garna lai
 export default app
--- src/config/config.ts
+++ src/config/config.ts
@@ -6,7 +6,8 @@ const envConfig = {
     port : process.env.PORT,
     mongodbString : process.env.MONGODB_URI,
     backendurl : process.env.BACKEND_URL,
-    enviornment : process.env.NODE_ENV
+    enviornment : process.env.NODE_ENV,
+    frontendurl : process.env.FRONTEND_URL
 }
 
 
